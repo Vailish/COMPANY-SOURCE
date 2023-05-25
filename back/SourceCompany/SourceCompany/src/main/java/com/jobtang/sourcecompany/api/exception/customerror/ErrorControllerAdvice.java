47004a2d91629bfdb45ef16ed430853ed30359e4
kimjinho-dev <kimjinho-dev@gmail.com>
2023-05-25 10:58:18 +0900

    feat : re add mm

--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/exception/customerror/ErrorControllerAdvice.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/exception/customerror/ErrorControllerAdvice.java
@@ -1,15 +1,21 @@
 package com.jobtang.sourcecompany.api.exception.customerror;
 
+import com.jobtang.sourcecompany.api.exception.mattermost.NotificationManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ErrorControllerAdvice {
+    @Autowired
+    private NotificationManager notificationManager;
 
 //    @ExceptionHandler(value = Exception.class)
 //    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -18,6 +24,13 @@ public class ErrorControllerAdvice {
 //        response.setDetail(e.getMessage());
 //        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 //    }
+
+    @ExceptionHandler(Exception.class)
+    public void exceptionTest(Exception e, HttpServletRequest req) {
+        e.printStackTrace();
+        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(value = NoSuchElementException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -32,10 +45,22 @@ public class ErrorControllerAdvice {
     /* Custom Error Handler */
     @ExceptionHandler(value = CustomException.class)
     @ResponseStatus(value = HttpStatus.ACCEPTED)
-    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e, HttpServletRequest req) {
+        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
         ErrorResponse response = ErrorResponse.of(e.getErrorCode());
         response.setDetail(e.getMessage());
 //        HttpStatus httpStatus = HttpStatus.valueOf(response.getCode());
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+    private String getParams(HttpServletRequest req) {
+        StringBuilder params = new StringBuilder();
+        Enumeration<String> keys = req.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append("\n");
+        }
+
+        return params.toString();
     }
 }
--- back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/exception/mattermost/GrobalControllerAdvice.java
+++ back/SourceCompany/SourceCompany/src/main/java/com/jobtang/sourcecompany/api/exception/mattermost/GrobalControllerAdvice.java
@@ -14,22 +14,23 @@ public class GrobalControllerAdvice {
     @Autowired
     private NotificationManager notificationManager;
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity exceptionTest(Exception e, HttpServletRequest req) {
-        e.printStackTrace();
-        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
-
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private String getParams(HttpServletRequest req) {
-        StringBuilder params = new StringBuilder();
-        Enumeration<String> keys = req.getParameterNames();
-        while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
-            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append("\n");
-        }
-
-        return params.toString();
-    }
+//    @ExceptionHandler(Exception.class)
+//    public void exceptionTest(Exception e, HttpServletRequest req) {
+//        e.printStackTrace();
+//        System.out.println("여기들어옴1");
+//        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
+//        System.out.println("여기들어옴2");
+////        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//
+//    private String getParams(HttpServletRequest req) {
+//        StringBuilder params = new StringBuilder();
+//        Enumeration<String> keys = req.getParameterNames();
+//        while (keys.hasMoreElements()) {
+//            String key = keys.nextElement();
+//            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append("\n");
+//        }
+//
+//        return params.toString();
+//    }
 }
